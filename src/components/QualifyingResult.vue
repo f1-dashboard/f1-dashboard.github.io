b0cd8e78ffcc8e93cf64fb6dc9821ecff909b9fc
Lucas Van Mol <lucas.vanmol@gmail.com>
2023-11-22 11:51:07 +0100

    Improve QualifyingResult

--- src/components/QualifyingResult.vue
+++ src/components/QualifyingResult.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2>Monza 2019 {{ qualifying }} Results</h2>
+        <h2>Monza 2023 {{ qualifying }} Results</h2>
         <div id="container"></div>
     </div>
 </template>
@@ -14,8 +14,7 @@ export default {
     props: ['qualifying'],
     watch: {
         qualifying: function (newVal, oldVal) {
-            this.render(newVal)
-            // TODO: use animations to change this nicely instead of reloading
+            this.update(newVal)
             console.log('Prop changed: ', newVal, ' | was: ', oldVal)
         }
     },
@@ -30,38 +29,70 @@ export default {
         this.render(this.qualifying)
     },
     methods: {
+        async update(q) {
+            const drivers = await this.getDriverData(q)
+
+            console.log(this.x)
+            // Update axis domains
+            this.x.domain([0, d3.max(drivers, d => d.delta)])
+            this.y.domain(d3.sort(drivers, d => d.delta).map(d => d.full_name))
+            // this.y.domain(d3.sort(drivers, d => d.delta).filter(d => !d.dnq).map(d => d.full_name))
+
+            const t = this.svg.transition().duration(750)
+            // transitions
+            this.gx.transition(t)
+                .call(d3.axisBottom(this.x))
+            this.gy.transition(t)
+                .call(d3.axisLeft(this.y))
+
+            let bars = this.svg.selectAll("rect")
+                .data(drivers)
+            bars.enter()
+                .append("rect")
+                .merge(bars)
+                .transition(t)
+                .attr("class", d => d.team)
+                .attr("x", this.x(0))
+                .attr("y", (d) => this.y(d.full_name))
+                .attr("width", (d) => this.x(d.delta) - this.x(0))
+                .attr("height", this.y.bandwidth())
+            bars.exit().remove()
+
+            let timing_labels = this.svg.selectAll(".delta")
+                .data(drivers)
+            timing_labels.enter()
+                .append("text")
+                .merge(timing_labels)
+                .attr("fill", "white")
+                .attr("text-anchor", "end")
+                .classed("delta", true)
+                .transition(t)
+                .attr("x", d => this.x(d.delta))
+                .attr("y", d => this.y(d.full_name) + this.y.bandwidth() / 2)
+                .attr("dy", "0.35em")
+                .attr("dx", -4)
+                .text(d => {
+                    if (d.delta < 0.0000001) {
+                        return d.time_string
+                    }
+                    return (d.delta < 0 ? "" : "+") + d.delta
+                }
+                )
+                .call(text => text.filter(d => this.x(d.delta) - this.x(0) < 50) // short bars
+                    .attr("dx", +4)
+                    .attr("fill", "black")
+                    .attr("text-anchor", "start"))
+
+            timing_labels.exit().remove()
+        },
+
         async render(q) {
             // Example taken from:
             // https://observablehq.com/@d3/horizontal-bar-chart/2?intent=fork
             // Improvements: use relative timings. e.g. delta from first
             // a cool idea would be to be able to click on someone and see their delta from others (turns into diverging bar chart)
             // https://observablehq.com/@d3/diverging-bar-chart/2?intent=fork
-            const drivers = await d3.csv("./data/monza_qualifying.csv", (d) => {
-                // Parse the duration string, e.g. "0 days 00:01:20.643000"
-                // const durationArray = d.Q1.split(" ");
-                const durationArray = d[q].split(" ");
-
-                try {
-                    // Extract days, hours, minutes, seconds, and milliseconds
-                    const days = parseInt(durationArray[0]);
-                    const timeComponents = durationArray[2].split(":");
-                    const hours = parseInt(timeComponents[0]);
-                    const minutes = parseInt(timeComponents[1]);
-                    const secondsArray = timeComponents[2].split(".");
-                    const seconds = parseInt(secondsArray[0]);
-                    const milliseconds = parseInt(secondsArray[1]) / 1000;
-                    return {
-                        full_name: d.FullName,
-                        q1_lap_time: hours * 3600 + minutes * 60 + seconds + milliseconds / 1000,
-                        time_string: `${minutes}:${seconds}.${milliseconds}`
-                    }
-                } catch {
-                    // Don't include drivers without lap times
-                    return null
-                }
-
-            });
-            console.log(drivers)
+            const drivers = await this.getDriverData(q)
 
             // Declare the chart dimensions and margins.
             const barHeight = 25;
@@ -70,11 +101,7 @@ export default {
             const marginBottom = 30;
             const marginLeft = 100;
             const width = 640;
-            const height = Math.ceil((drivers.length + 0.1) * barHeight) + marginTop + marginBottom;
-
-            // Choose the fastest time to calculate the delta
-            const relativeTime = d3.min(drivers, d => d.q1_lap_time)
-            drivers.forEach(d => d.delta = (d.q1_lap_time - relativeTime).toFixed(3))
+            const height = 500;
 
             // Declare the x (horizontal position) scale.
             const x = d3.scaleLinear()
@@ -95,29 +122,30 @@ export default {
                 .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
 
             // Create the bar for each driver
-            svg.append("g")
-                .attr("fill", "steelblue")
+            this.bars = svg.append("g")
                 .selectAll()
                 .data(drivers)
                 .join("rect")
+                .attr("class", d => d.team)
                 .attr("x", x(0))
                 .attr("y", (d) => y(d.full_name))
                 .attr("width", (d) => x(d.delta) - x(0))
                 .attr("height", y.bandwidth())
 
             // Add the timing label 00:00.000
-            svg.append("g")
+            this.timing_labels = svg.append("g")
                 .attr("fill", "white")
                 .attr("text-anchor", "end")
                 .selectAll()
                 .data(drivers)
                 .join("text")
+                .classed("delta", true)
                 .attr("x", d => x(d.delta))
                 .attr("y", d => y(d.full_name) + y.bandwidth() / 2)
                 .attr("dy", "0.35em")
                 .attr("dx", -4)
                 .text(d => {
-                    if (d.q1_lap_time === relativeTime) {
+                    if (d.delta < 0.0000001) {
                         return d.time_string
                     }
                     return (d.delta < 0 ? "" : "+") + d.delta
@@ -129,14 +157,18 @@ export default {
                     .attr("text-anchor", "start"))
 
             // Add the x-axis.
-            svg.append("g")
+            this.gx = svg.append("g")
                 .attr("transform", `translate(0,${height - marginBottom})`)
                 .call(d3.axisBottom(x));
 
             // Add the y-axis.
-            svg.append("g")
+            this.gy = svg.append("g")
                 .attr("transform", `translate(${marginLeft},0)`)
                 .call(d3.axisLeft(y));
+
+            this.x = x;
+            this.y = y;
+            this.svg = svg;
 
             // Append the SVG element. 
             if (container.childNodes.length > 0) {
@@ -145,8 +177,55 @@ export default {
                 container.append(svg.node())
             }
         },
+
+        async getDriverData(q) {
+            const drivers = await d3.csv("./data/monza_qualifying_2023.csv", (d) => {
+                // Parse the duration string, e.g. "0 days 00:01:20.643000"
+                // const durationArray = d.Q1.split(" ");
+                const durationArray = d[q].split(" ");
+
+                try {
+                    // Extract days, hours, minutes, seconds, and milliseconds
+                    const days = parseInt(durationArray[0]);
+                    const timeComponents = durationArray[2].split(":");
+                    const hours = parseInt(timeComponents[0]);
+                    const minutes = parseInt(timeComponents[1]);
+                    const secondsArray = timeComponents[2].split(".");
+                    const seconds = parseInt(secondsArray[0]);
+                    const milliseconds = parseInt(secondsArray[1]) / 1000;
+                    return {
+                        full_name: d.FullName,
+                        q1_lap_time: hours * 3600 + minutes * 60 + seconds + milliseconds / 1000,
+                        time_string: `${minutes}:${seconds}.${milliseconds}`,
+                        dnq: false,
+                        team: d.TeamId
+                    }
+                } catch {
+                    // Don't include drivers without lap times
+                    return {
+                        full_name: d.FullName,
+                        q1_lap_time: 0,
+                        time_string: `DNQ`,
+                        dnq: true,
+                        team: d.TeamId
+                    }
+                }
+
+            });
+            console.log(drivers)
+
+            // Choose the fastest time to calculate the delta
+            const driversWithTimes = d3.filter(drivers, d => !d.dnq)
+            const relativeTime = d3.min(driversWithTimes, d => d.q1_lap_time)
+            drivers.forEach(d => d.delta = (d.q1_lap_time - relativeTime).toFixed(3))
+            return d3.filter(drivers, d => !d.dnq)
+        }
     }
 };
 </script>
 
-<style></style>
+<style>
+/* rect {
+    fill: steelblue
+} */
+</style>
